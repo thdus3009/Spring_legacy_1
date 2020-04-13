98276c1556f769fc18e4c1277e3b9533fb759be9
thdus3009 <63095234+thdus3009@users.noreply.github.com>
2020-04-13 16:38:59 +0900

    add

--- src/main/java/com/sy/spring_1/SubController.java
+++ src/main/java/com/sy/spring_1/SubController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class SubController {
 	@RequestMapping(value="sub1")
-	public String sub1() {
-		return "sub/sub1";
-	}
+	public void sub1() {
+	
+	} //요청하는 return값과 내가 가야되는 경로명이 같다면 굳이 return할 필요없이 void를 쓰면 된다. 
 	
 	@RequestMapping(value="sub2")
 	public String sub2() {
